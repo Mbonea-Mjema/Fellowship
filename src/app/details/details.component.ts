113f293bdddecc368d5c0437e72eff445500855a
Mbonea-Mjema <mbonegd@yahoo.com>
2019-03-19 12:48:04 +0530

    added data base and removed somefiles

--- src/app/details/details.component.ts
+++ src/app/details/details.component.ts
@@ -1,40 +1,48 @@
-import { Component, OnInit } from '@angular/core';
-import { DataService } from '../Services/data.service'
-import { FirebaseService } from '../Services/firebase.service'
+import { Component, OnInit } from "@angular/core";
+import { DataService } from "../Services/data.service";
+import { FirebaseService } from "../Services/firebase.service";
 @Component({
-  selector: 'app-details',
-  templateUrl: './details.component.html',
-  styleUrls: ['./details.component.css']
+  selector: "app-details",
+  templateUrl: "./details.component.html",
+  styleUrls: ["./details.component.css"]
 })
 export class DetailsComponent implements OnInit {
-
-  constructor(private transfer: DataService, private db:FirebaseService) { }
+  constructor(private transfer: DataService, private db: FirebaseService) {}
   Details: object;
   user: object;
   info: object;
+  size = "Size";
+  Sizes = ["SMALL", "MEDIUM", "LARGE", "XXL"];
   ngOnInit() {
-    
     //product data
     this.transfer.Data.subscribe(data => {
-      this.Details =
-        {
-          'name': data.name,
-          'image': data.image,
-        }
-      console.log(this.Details)
-    })
-    
+      this.Details = {
+        name: data.name,
+        image: data.image
+      };
+      console.log(this.Details);
+    });
+
     //user data
-    this.db.User.subscribe(data=>{
-      this.user=data;
-    })
+    this.db.User.subscribe(data => {
+      this.user = data;
+      console.log(this.user);
+    });
+    console.log(this.user);
+  }
+  setSize(s) {
+    this.size = s;
   }
 
   Order() {
-  let size:any= document.getElementById("size");
-  console.log(size);
+   
+    console.log(this.size);
+    console.log(this.user);
 
-
+    this.db.dataBase.collection("Orders").doc(this.user.user.displayName).set({
+      name:this.user.user.displayName,
+      size:this.size,
+      type:this.Details.name
+  })
   }
-
 }
