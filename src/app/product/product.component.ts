8f192a4ce5f580c5dfb2bf4d80068c4982452716
Mbonea-Mjema <mjema86@gmail.com>
2019-03-19 13:39:08 +0530

    old changes

--- src/app/product/product.component.ts
+++ src/app/product/product.component.ts
@@ -15,6 +15,7 @@ export class ProductComponent implements OnInit {
   }
  Buy(img:string,name:string){
   let image:any= document.getElementById(img).src;
+  console.log(image)
   let detail= document.getElementById(name).textContent;
   let data:object={'image':image,'name':detail}
   this.tranfer.Data.next(data);
